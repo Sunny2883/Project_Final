0352f81a703117cf8f440ee15549cb67f2fccd25
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-03-18 12:45:48 +0530

    Changes service name and ip

--- CustomerSuccessPlatformClient/src/environments/environment.development.ts
+++ CustomerSuccessPlatformClient/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl : "https://13.232.240.217:8080",
-    clientURL : "https://13.232.240.217:4200/",
+    apiUrl : "https://13.233.150.195:8080",
+    clientURL : "https://13.233.150.195:4200/",
     AUTH0_DOMAIN:'dev-w2cpuspyl8y04bzh.us.auth0.com',
     AUTH0_CLIENT_ID:'l6AoZ9MlugUZmmFY83P58BA2YQargRJ3',
 };
