c15752b80caa46830005023720e763b074407dc1
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-03-18 20:31:39 +0530

    Adds worklow of staging branch

--- CustomerSuccessPlatformClient/src/environments/environment.development.ts
+++ CustomerSuccessPlatformClient/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl : "http://13.233.150.195:8080",
-    clientURL : "https://13.233.150.195:4200/",
+    apiUrl : "http://65.2.153.112:8080",
+    clientURL : "https://65.2.153.112:4200/",
     AUTH0_DOMAIN:'dev-ekehvbfll57tols1.us.auth0.com',
     AUTH0_CLIENT_ID:'BGdkQ7i8w9l9ZMAxAWe8EZg13kPmvE0O',
 };
