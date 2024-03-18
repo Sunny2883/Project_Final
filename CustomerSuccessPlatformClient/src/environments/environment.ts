c0e64d45f7cd2c31ddddb9a2e5a6c5166f1b4f06
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-03-18 18:53:01 +0530

    Adds action workflow in dev

--- CustomerSuccessPlatformClient/src/environments/environment.ts
+++ CustomerSuccessPlatformClient/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl : "http://13.233.150.195:8080",
-    clientURL : "https://13.233.150.195:4200",
+    apiUrl : "http://13.201.84.172:8080",
+    clientURL : "https://13.201.84.172:4200",
     AUTH0_DOMAIN:'dev-ekehvbfll57tols1.us.auth0.com',
     AUTH0_CLIENT_ID:'BGdkQ7i8w9l9ZMAxAWe8EZg13kPmvE0O',
 };
