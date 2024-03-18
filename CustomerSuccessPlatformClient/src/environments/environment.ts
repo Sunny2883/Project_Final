2626595c3459cc3e99747ce69c08983a8fc673a4
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-03-18 13:27:04 +0530

    changes cpu and memory size in task definition

--- CustomerSuccessPlatformClient/src/environments/environment.ts
+++ CustomerSuccessPlatformClient/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     apiUrl : "http://13.233.150.195:8080",
     clientURL : "https://13.233.150.195:4200",
-    AUTH0_DOMAIN:'dev-w2cpuspyl8y04bzh.us.auth0.com',
-    AUTH0_CLIENT_ID:'l6AoZ9MlugUZmmFY83P58BA2YQargRJ3',
+    AUTH0_DOMAIN:'dev-ekehvbfll57tols1.us.auth0.com',
+    AUTH0_CLIENT_ID:'BGdkQ7i8w9l9ZMAxAWe8EZg13kPmvE0O',
 };
