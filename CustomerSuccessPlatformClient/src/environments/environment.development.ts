bd17a58af89f0e0496ee75996be69c07b4965d7f
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-04-03 18:40:30 +0530

    Initial commit

--- CustomerSuccessPlatformClient/src/environments/environment.development.ts
+++ CustomerSuccessPlatformClient/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl : "http://cspprod.jainil.me",
-    clientURL : "https://cspprod.jainil.me/",
+    apiUrl : "http://loadbalancer-135651691.ap-south-1.elb.amazonaws.com:8080",
+    clientURL : "https://loadbalancer-135651691.ap-south-1.elb.amazonaws.com",
     AUTH0_DOMAIN:'dev-ekehvbfll57tols1.us.auth0.com',
     AUTH0_CLIENT_ID:'BGdkQ7i8w9l9ZMAxAWe8EZg13kPmvE0O',
 };
