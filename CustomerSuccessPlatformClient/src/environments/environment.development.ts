a2e70e24dde61869b470fde4114e8c2410a8ef97
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-04-03 15:55:59 +0530

    Commit for check by mentor

--- CustomerSuccessPlatformClient/src/environments/environment.development.ts
+++ CustomerSuccessPlatformClient/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl : "http://13.233.114.9:8080",
-    clientURL : "https://13.233.114.9:4200/",
+    apiUrl : "http://13.127.110.142:8080",
+    clientURL : "https://13.127.110.142:4200/",
     AUTH0_DOMAIN:'dev-ekehvbfll57tols1.us.auth0.com',
     AUTH0_CLIENT_ID:'BGdkQ7i8w9l9ZMAxAWe8EZg13kPmvE0O',
 };
