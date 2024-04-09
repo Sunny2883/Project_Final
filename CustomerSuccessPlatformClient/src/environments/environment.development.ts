00e51f0414b733deefae87043cf55f627281671f
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-04-09 17:33:36 +0530

    Changes api url

--- CustomerSuccessPlatformClient/src/environments/environment.development.ts
+++ CustomerSuccessPlatformClient/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl : "https://cspstg.jainil.me",
+    apiUrl : "https://cspstg.jainil.me:8080",
     clientURL : "https://cspstg.jainil.me/",
     AUTH0_DOMAIN:'dev-ekehvbfll57tols1.us.auth0.com',
     AUTH0_CLIENT_ID:'BGdkQ7i8w9l9ZMAxAWe8EZg13kPmvE0O',
