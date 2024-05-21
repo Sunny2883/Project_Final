080e8986660f24692231588d3eb4151a253a7695
SUNNY BHUSHAN <sunnybhushan9185@gmail.com>
2024-05-21 19:50:03 +0530

    initial commit

--- CustomerSuccessPlatformClient/src/environments/environment.development.ts
+++ CustomerSuccessPlatformClient/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl : "https://cspstg.jainil.me:8080",
-    clientURL : "https://cspstg.jainil.me/",
+    apiUrl : "https://csptest.jainil.me:8080",
+    clientURL : "https://csptest.jainil.me/",
     AUTH0_DOMAIN:'dev-ekehvbfll57tols1.us.auth0.com',
     AUTH0_CLIENT_ID:'BGdkQ7i8w9l9ZMAxAWe8EZg13kPmvE0O',
 };
